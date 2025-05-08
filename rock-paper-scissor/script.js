7d1614a686df7a5666f270801b0095446eefd568
Vincent <vincentbolinget@gmail.com>
2025-05-08 22:31:33 +0800

    Add playRound function

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -1,18 +1,43 @@
 let humanScore = 0;
 let computerScore = 0;
 
-const choices = ['rock', 'paper', 'scissors'];
-
-function getRandomInt(max){
+function getRandomInt(max) {
     return Math.floor(Math.random() * max)
 }
 
-function getComputerChoice(){
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
     let choiceIndex = getRandomInt(choices.length);
     return choices[choiceIndex];
 }
 
-function getHumanChoice(){
+function getHumanChoice() {
     let humanChoice = prompt('Rock, Paper or Scissor? ');
     return humanChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice === computerChoice){
+        alert("DRAW");
+        return;
+    }else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        computerScore += 1;
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        humanScore += 1;
+    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        computerScore += 1;
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        humanScore += 1;
+    } else if (humanChoice === 'scissor' && computerChoice === 'rock') {
+        computerScore += 1;
+    } else if (humanChoice === 'rock' && computerChoice === 'scissor') {
+        computerScore += 1;
+    }
+
+    alert(`Computer Score: ${computerScore}, Human Score: ${humanScore}`)
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
